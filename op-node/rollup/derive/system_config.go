502d6141545930744a4e440c1bfbf13f846d4c35
mtgnoah <noahpravecek@gmail.com>
2023-11-01 09:32:48 -0500

    Fixes

--- op-node/rollup/derive/system_config.go
+++ op-node/rollup/derive/system_config.go
@@ -21,6 +21,7 @@ var (
 	SystemConfigUpdateGasConfig         = common.Hash{31: 1}
 	SystemConfigUpdateGasLimit          = common.Hash{31: 2}
 	SystemConfigUpdateUnsafeBlockSigner = common.Hash{31: 3}
+	SystemConfigUpdateNodeKit           = common.Hash{31: 4}
 )
 
 var (
@@ -132,6 +133,30 @@ func ProcessSystemConfigUpdateLogEvent(destSysCfg *eth.SystemConfig, ev *types.L
 		return nil
 	case SystemConfigUpdateUnsafeBlockSigner:
 		// Ignored in derivation. This configurable applies to runtime configuration outside of the derivation.
+		return nil
+	case SystemConfigUpdateNodeKit:
+		if pointer, err := solabi.ReadUint64(reader); err != nil || pointer != 32 {
+			return NewCriticalError(errors.New("invalid pointer field"))
+		}
+		if length, err := solabi.ReadUint64(reader); err != nil || length != 32 {
+			return NewCriticalError(errors.New("invalid length field"))
+		}
+		nodekit, err := solabi.ReadEthBytes32(reader)
+		if err != nil {
+			return NewCriticalError(errors.New("could not read nodekit"))
+		}
+		if !solabi.EmptyReader(reader) {
+			return NewCriticalError(errors.New("too many bytes"))
+		}
+		for i := 0; i < 31; i++ {
+			if nodekit[i] != 0 {
+				return NewCriticalError(errors.New("invalid nodekit value"))
+			}
+		}
+		if nodekit[31] != 0 && nodekit[31] != 1 {
+			return NewCriticalError(errors.New("invalid nodekit value"))
+		}
+		destSysCfg.NodeKit = (nodekit[31] == 1)
 		return nil
 	default:
 		return fmt.Errorf("unrecognized L1 sysCfg update type: %s", updateType)
--- packages/contracts-bedrock/scripts/Deploy.s.sol
+++ packages/contracts-bedrock/scripts/Deploy.s.sol
@@ -43,6 +43,15 @@ import "src/libraries/DisputeTypes.sol";
 ///         deployment so that hardhat-deploy style artifacts can be generated using a call to `sync()`.
 contract Deploy is Deployer {
     DeployConfig cfg;
+    bytes32 implSalt;
+
+    /// @notice The create2 salt used for deployment of the contract implementations.
+    ///         Using this helps to reduce config across networks as the implementation
+    ///         addresses will be the same across networks when deployed with create2.
+    ///         This can be overridden by setting `IMPL_SALT` in the environment, which
+    ///         allows the creation of multiple simultaneous deployments with contracts
+    ///         at different addresses.
+    bytes32 constant IMPL_SALT = keccak256(bytes("ether's phoenix"));
 
     /// @notice The name of the script, used to ensure the right deploy artifacts
     ///         are used.
@@ -56,8 +65,11 @@ contract Deploy is Deployer {
         string memory path = string.concat(vm.projectRoot(), "/deploy-config/", deploymentContext, ".json");
         cfg = new DeployConfig(path);
 
+        implSalt = vm.envOr("IMPL_SALT", IMPL_SALT);
+
         console.log("Deploying from %s", deployScript);
         console.log("Deployment context: %s", deploymentContext);
+        console.logBytes32(implSalt);
     }
 
     /// @notice Deploy all of the L1 contracts
@@ -94,6 +106,18 @@ contract Deploy is Deployer {
     modifier onlyDevnet() {
         uint256 chainid = block.chainid;
         if (chainid == Chains.LocalDevnet || chainid == Chains.GethDevnet) {
+            _;
+        }
+    }
+
+    /// @notice Modifier that will only allow a function to be called on a public
+    ///         testnet or devnet.
+    modifier onlyTestnetOrDevnet() {
+        uint256 chainid = block.chainid;
+        if (
+            chainid == Chains.Goerli || chainid == Chains.Sepolia || chainid == Chains.LocalDevnet
+                || chainid == Chains.GethDevnet
+        ) {
             _;
         }
     }
@@ -288,7 +312,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the L1CrossDomainMessenger
     function deployL1CrossDomainMessenger() public broadcast returns (address addr_) {
-        L1CrossDomainMessenger messenger = new L1CrossDomainMessenger();
+        L1CrossDomainMessenger messenger = new L1CrossDomainMessenger{ salt: implSalt }();
 
         require(address(messenger.PORTAL()) == address(0));
         require(address(messenger.portal()) == address(0));
@@ -304,7 +328,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the OptimismPortal
     function deployOptimismPortal() public broadcast returns (address addr_) {
-        OptimismPortal portal = new OptimismPortal();
+        OptimismPortal portal = new OptimismPortal{ salt: implSalt }();
 
         require(address(portal.L2_ORACLE()) == address(0));
         require(portal.GUARDIAN() == address(0));
@@ -319,7 +343,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the L2OutputOracle
     function deployL2OutputOracle() public broadcast returns (address addr_) {
-        L2OutputOracle oracle = new L2OutputOracle({
+        L2OutputOracle oracle = new L2OutputOracle{ salt: implSalt }({
             _submissionInterval: cfg.l2OutputOracleSubmissionInterval(),
             _l2BlockTime: cfg.l2BlockTime(),
             _finalizationPeriodSeconds: cfg.finalizationPeriodSeconds()
@@ -346,7 +370,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the OptimismMintableERC20Factory
     function deployOptimismMintableERC20Factory() public broadcast returns (address addr_) {
-        OptimismMintableERC20Factory factory = new OptimismMintableERC20Factory();
+        OptimismMintableERC20Factory factory = new OptimismMintableERC20Factory{ salt: implSalt }();
 
         require(factory.BRIDGE() == address(0));
         require(factory.bridge() == address(0));
@@ -359,7 +383,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the DisputeGameFactory
     function deployDisputeGameFactory() public onlyDevnet broadcast returns (address addr_) {
-        DisputeGameFactory factory = new DisputeGameFactory();
+        DisputeGameFactory factory = new DisputeGameFactory{ salt: implSalt }();
         save("DisputeGameFactory", address(factory));
         console.log("DisputeGameFactory deployed at %s", address(factory));
 
@@ -368,7 +392,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the BlockOracle
     function deployBlockOracle() public onlyDevnet broadcast returns (address addr_) {
-        BlockOracle oracle = new BlockOracle();
+        BlockOracle oracle = new BlockOracle{ salt: implSalt }();
         save("BlockOracle", address(oracle));
         console.log("BlockOracle deployed at %s", address(oracle));
 
@@ -377,7 +401,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the PreimageOracle
     function deployPreimageOracle() public onlyDevnet broadcast returns (address addr_) {
-        PreimageOracle preimageOracle = new PreimageOracle();
+        PreimageOracle preimageOracle = new PreimageOracle{ salt: implSalt }();
         save("PreimageOracle", address(preimageOracle));
         console.log("PreimageOracle deployed at %s", address(preimageOracle));
 
@@ -386,7 +410,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy Mips
     function deployMips() public onlyDevnet broadcast returns (address addr_) {
-        MIPS mips = new MIPS(IPreimageOracle(mustGetAddress("PreimageOracle")));
+        MIPS mips = new MIPS{ salt: implSalt }(IPreimageOracle(mustGetAddress("PreimageOracle")));
         save("Mips", address(mips));
         console.log("MIPS deployed at %s", address(mips));
 
@@ -395,7 +419,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the SystemConfig
     function deploySystemConfig() public broadcast returns (address addr_) {
-        SystemConfig config = new SystemConfig();
+        SystemConfig config = new SystemConfig{ salt: implSalt }();
 
         require(config.owner() == address(0xdEaD));
         require(config.overhead() == 0);
@@ -428,7 +452,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the L1StandardBridge
     function deployL1StandardBridge() public broadcast returns (address addr_) {
-        L1StandardBridge bridge = new L1StandardBridge();
+        L1StandardBridge bridge = new L1StandardBridge{ salt: implSalt }();
 
         require(address(bridge.MESSENGER()) == address(0));
         require(address(bridge.messenger()) == address(0));
@@ -443,7 +467,7 @@ contract Deploy is Deployer {
 
     /// @notice Deploy the L1ERC721Bridge
     function deployL1ERC721Bridge() public broadcast returns (address addr_) {
-        L1ERC721Bridge bridge = new L1ERC721Bridge();
+        L1ERC721Bridge bridge = new L1ERC721Bridge{ salt: implSalt }();
 
         require(address(bridge.MESSENGER()) == address(0));
         require(bridge.OTHER_BRIDGE() == Predeploys.L2_ERC721_BRIDGE);
